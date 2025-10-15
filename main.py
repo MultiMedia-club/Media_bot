f4eb17890917e32fd120ce4cd07d4bff8923ced7
neito330-alt <neito330.pc@gmail.com>
2025-10-16 06:41:33 +0900

    update

--- main.py
+++ main.py
@@ -121,5 +121,5 @@ class MediaBot(commands.Bot):
             await interaction.followup.send('失敗しました',ephemeral=True)
 
         
-server_thread(os.environ['SERVICE_PORT'])
+server_thread(int(os.environ['SERVICE_PORT']))
 bot = MediaBot()
